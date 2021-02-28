b43dbffe1ab31973c6b6a2235be4013bbcc5fd11
jacob-lcs <jacob.lcs@qq.com>
2021-02-28 11:25:35 +0800

    feat: style 样式文件解析

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.less';
+import less from './index.module.less';
+import stylus from './index.module.styl';
+import scss from './index.module.scss';
+import css from './index.module.css';
+
 ReactDOM.render(
   <React.StrictMode>
-    <div>你好，React-webpack5-template</div>
+    <div className="title">你好，React-webpack5-template</div>
+    <div className={css.css}>css</div>
+    <div className={scss.scss}>scss</div>
+    <div className={less.font}>less</div>
+    <div className={stylus.stylus}>stylus</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const getCSSModuleLocalIdent = require("react-dev-utils/getCSSModuleLocalIdent");
+const webpack = require("webpack");
+
+// css/css module 正则表达式
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+// sass/sass module 正则表达式
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+// less/less module 正则表达式
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
+// stylus/stylus module 正则表达式
+const stylRegex = /\.styl$/;
+const stylModuleRegex = /\.module\.styl$/;
 
 module.exports = () => {
   return {
@@ -17,6 +32,9 @@ module.exports = () => {
         filename: "index.html",
         template: "./build/index.html",
       }),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
     ],
     module: {
       rules: [
@@ -25,6 +43,90 @@ module.exports = () => {
           use: ["babel-loader"],
           include: path.resolve(__dirname, 'src'),
         },
+        {
+          test: cssRegex,
+          exclude: cssModuleRegex,
+          use: ["style-loader", "css-loader", "postcss-loader"]
+        },
+        {
+          test: cssModuleRegex,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  getLocalIdent: getCSSModuleLocalIdent,
+                }
+              }
+            },
+            "postcss-loader"
+          ]
+        },
+        {
+          test: sassRegex,
+          exclude: sassModuleRegex,
+          use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+        },
+        {
+          test: sassModuleRegex,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  getLocalIdent: getCSSModuleLocalIdent,
+                }
+              }
+            },
+            "postcss-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: stylRegex,
+          exclude: stylModuleRegex,
+          use: ["style-loader", "css-loader", "postcss-loader", "stylus-loader"]
+        },
+        {
+          test: stylModuleRegex,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  getLocalIdent: getCSSModuleLocalIdent,
+                }
+              }
+            },
+            "postcss-loader",
+            "stylus-loader"
+          ]
+        },
+        {
+          test: lessRegex,
+          exclude: lessModuleRegex,
+          use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
+          sideEffects: true,
+        },
+        {
+          test: lessModuleRegex,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  getLocalIdent: getCSSModuleLocalIdent,
+                }
+              }
+            },
+            "postcss-loader",
+            "less-loader"
+          ],
+        }
       ]
     },
     devServer: {
